b7b868f338034e1d0e663129247917f3410b421f
Krzysztof Darasz <krzysztof.darasz@brightpearl.com>
2020-03-22 22:30:13 +0000

    first simple list

---
+++ src/first.rs
@@ -0,0 +1,80 @@
+use std::mem;
+
+pub struct List{
+    head: Link
+}
+
+enum Link{
+    Empty,
+    More(Box<Node>)
+}
+
+struct Node{
+    elem: i32,
+    next: Link
+}
+
+impl List{
+
+    pub fn new() -> Self{
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32){
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty )
+        });
+        self.head = Link::More(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+            },
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            }
+        };
+        return result;
+    }
+}
+
+impl Drop for List{
+    fn drop(&mut self) {
+        let mut curr_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = curr_link {
+            curr_link = mem::replace(&mut boxed_node.next,Link::Empty)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use super::List;
+
+    #[test]
+    fn basics(){
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(std::mem::drop(list), ());
+    }
+}
