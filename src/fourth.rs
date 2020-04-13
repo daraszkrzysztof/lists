93963a4e1450179a23467f332aca52048954579c
Krzysztof Darasz <krzysztof.darasz@brightpearl.com>
2020-04-13 23:36:05 +0100

    4th

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,47 +1,47 @@
-use std::cell::RefCell;
+use std::cell::{RefCell, Ref};
 use std::rc::Rc;
 
-pub struct List<T>{
+pub struct List<T> {
     head: Link<T>,
-    tail: Link<T>
+    tail: Link<T>,
 }
 
 type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
-struct Node<T>{
+struct Node<T> {
     elem: T,
     prev: Link<T>,
-    next: Link<T>
+    next: Link<T>,
 }
 
-impl<T> Node<T>{
-    fn new(elem: T) -> Rc<RefCell<Node<T>>>{
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Node<T>>> {
         Rc::new(
             RefCell::new(
-                Node{
+                Node {
                     prev: None,
                     next: None,
-                    elem: elem
+                    elem: elem,
                 }
             )
         )
     }
 }
 
-impl<T> List<T>{
-    fn new() ->Self{
-        List{
+impl<T> List<T> {
+    fn new() -> Self {
+        List {
             head: None,
-            tail: None
+            tail: None,
         }
     }
 
-    pub fn push_front(&mut self, elem: T){
+    pub fn push_front(&mut self, elem: T) {
         let new_head = Node::new(elem);
         match self.head.take() {
             Some(old_head) => {
-                old_head.prev = Some(new_head.clone());
-                new_head.next = Some(old_head);
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
                 self.head = Some(new_head)
             }
             None => {
@@ -49,5 +49,60 @@ impl<T> List<T>{
                 self.head = Some(new_head);
             }
         }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
